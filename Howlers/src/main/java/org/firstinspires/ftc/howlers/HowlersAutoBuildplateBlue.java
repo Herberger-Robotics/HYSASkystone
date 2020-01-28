652f3529a5a559df3f3a7f1fb037001b0c1ce8f6
Josh Zakaras <joshapps027@gmail.com>
2020-01-28 16:20:59 -0700

    here's my code :)

--- Howlers/src/main/java/org/firstinspires/ftc/howlers/HowlersAutoBuildplateBlue.java
+++ Howlers/src/main/java/org/firstinspires/ftc/howlers/HowlersAutoBuildplateBlue.java
@@ -24,13 +24,13 @@ public class HowlersAutoBuildplateBlue extends HowlersAutoFunction {
         encoderDrive(speed, 0.3, 0.3);//forwards
         encoderLift(speed,-1);//lower
         encoderDrive(speed,-1.3,-1.3);//backwards
-        encoderLift(speed,1);//raise
+        encoderDrive(speed,-0.7,-0.5);//backwards
+        encoderDrive(speed,0.2,0.2);//forwards
         encoderStrafe(speed,-1,1);//right
-        encoderLift(speed,-1);//lower
         encoderStrafe(speed,-0.7,0.7);//right
         encoderDrive(speed,1.3,1.3); //forwards
         encoderStrafe(speed,1,-1); //left
-        encoderTurn(speed,1.3,1.3);//turnleft
-        encoderDrive(speed,1,1);
+        encoderLift(speed,1);//raise
+        encoderDrive(speed,-0.5,-0.5);//backwards
     }
 }
